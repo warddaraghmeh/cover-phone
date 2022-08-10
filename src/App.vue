5ce8581f5bb73a40e01ec206923e1f7c57a5ab1f
warddaraghmeh <109684022+warddaraghmeh@users.noreply.github.com>
2022-08-10 16:14:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import coverSkins from './components/cover-skins.vue'
+
+export default defineComponent({
+  components: { coverSkins },
+  setup() {
+    
+  },
+})
+</script>
+<template>
+  <cover-skins></cover-skins>
+</template>
+
+<style scoped>
+
+</style>
